a408e50f0be290b0592a16eb8da6efcf9cede2e8
Rawan Ahmed <62115135+raawanahmed@users.noreply.github.com>
2023-05-28 04:00:55 +0300

    add fav posts page

--- posts-app/src/app/favPosts/page.tsx
+++ posts-app/src/app/favPosts/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import styles from "./page.module.css";
+import styles from "../page.module.css";
 import { usePostContext } from "@/context/context";
 import ListOfPosts from "@/components/ListOfPosts";
+
 export default function Home() {
   const { dummyPosts } = usePostContext();
+  const favoritePosts = dummyPosts.filter((post) => post.isPostFav);
+
   return (
     <div className={styles.layout}>
-      <ListOfPosts postsList={dummyPosts}></ListOfPosts>
+      <ListOfPosts postsList={favoritePosts} />
     </div>
   );
 }
